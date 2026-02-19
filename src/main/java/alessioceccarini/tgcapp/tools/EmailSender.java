b293dceab892653f99cde599168e169dd52c5c1f
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-19 12:14:35 +0100

    added emailsender tool and added dep

---
+++ src/main/java/alessioceccarini/tgcapp/tools/EmailSender.java
@@ -0,0 +1,31 @@
+package alessioceccarini.tgcapp.tools;
+
+
+import alessioceccarini.tgcapp.entities.User;
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
+import kong.unirest.core.Unirest;
+import org.springframework.beans.factory.annotation.Value;
+
+
+public class EmailSender {
+	private String apiKey;
+	private String domain;
+
+	public EmailSender(@Value("${mg.apikey}") String apiKey, @Value("${mg.domain}") String domain) {
+		this.apiKey = apiKey;
+		this.domain = domain;
+	}
+
+	public void sendEmail(User user) { //TODO: METTERE IN FASE DI CREAZIONE NUOVO UTENTE E COMUNICAZIONE TRADE
+
+		HttpResponse<JsonNode> response = Unirest.post(
+						"https://api.mailgun.net/v3/" + this.domain + "/email")
+				.basicAuth("api", this.apiKey)
+				.queryString("to", user.getEmail())
+				.queryString("from", user.getEmail())
+				.queryString("subject", user.getFirstName() + " " + user.getLastName())
+				.queryString("text", user.getFirstName() + " " + user.getLastName()).asJson();
+
+	}
+}
