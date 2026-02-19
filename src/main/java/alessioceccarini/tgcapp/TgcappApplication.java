a8534811a69cd30418e3407f18d766c8263deb5e
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-19 11:19:36 +0100

    cretion of entities and enums

---
+++ src/main/java/alessioceccarini/tgcapp/TgcappApplication.java
@@ -0,0 +1,13 @@
+package alessioceccarini.tgcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgcappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgcappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessioceccarini/tgcapp/TgcappApplicationTests.java
@@ -0,0 +1,13 @@
+package alessioceccarini.tgcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
