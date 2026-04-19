25e662542ffd600b8bce5add70f3aea1eaa976d2
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-04-19 17:18:04 +0200

    fix chat controller

--- src/main/java/alessioceccarini/tgcapp/controllers/ChatController.java
+++ src/main/java/alessioceccarini/tgcapp/controllers/ChatController.java
@@ -38,8 +38,14 @@ public class ChatController {
 		System.out.println("SENDER: " + message.sender());
 		System.out.println("RECEIVER: " + message.receiver());
 
-		simpMessagingTemplate.convertAndSend(
-				"/topic/private/" + message.receiver(),
+		simpMessagingTemplate.convertAndSendToUser(
+				message.receiver(),
+				"/queue/messages",
+				message
+		);
+		simpMessagingTemplate.convertAndSendToUser(
+				message.sender(),
+				"/queue/messages",
 				message
 		);
 
